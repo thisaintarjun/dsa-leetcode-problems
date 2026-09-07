4103afd3c320a766ca85e56a4e690582796d3499
thisaintarjun <arjunsinghgodara09@gmail.com>
2026-09-08 00:10:56 +0530

    Time: 79 ms (5.24%), Space: 222.7 MB (5.03%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,22 +13,20 @@ class Solution {
 private:
     TreeNode* buildTree(vector<int>preorder, int prestart,int preend,vector<int>inorder, int instart, int inend, map<int, int>& inMap ){
         if(instart>inend||prestart>preend) return NULL;
-
-        TreeNode* root= new TreeNode(preorder[prestart]);
-        int inroot=inMap[root->val];
-        int numsleft=inroot-instart;
-        root->left=buildTree(preorder, prestart+1, preend, inorder, instart,inroot-1,inMap);
-        root->right=buildTree(preorder, prestart+numsleft+1, preend, inorder,inroot+1,inend,inMap);
-
+        TreeNode* root=new TreeNode(preorder[prestart]);
+        int inRoot=inMap[root->val];
+        int numsLeft=inRoot-instart;
+        root->left= buildTree(preorder, prestart+1, preend, inorder, instart, inRoot-1, inMap);
+        root->right=buildTree(preorder, prestart+numsLeft+1,preend,inorder, inRoot+1,inend, inMap);
         return root;
     }
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        map<int, int> inMap;
+        map<int, int>inMap;
         for(int i=0;i<inorder.size();i++){
             inMap[inorder[i]]=i;
         }
-        TreeNode* root=buildTree(preorder, 0, preorder.size()-1,inorder,0, inorder.size()-1,inMap);
+        TreeNode* root=buildTree(preorder, 0, preorder.size()-1,inorder, 0, inorder.size()-1, inMap);
         return root;
     }
 };
